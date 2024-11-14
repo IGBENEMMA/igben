31d9035b3893b645b7b2d13832d7a141d84cea5b
IGBENEMMA <165777994+IGBENEMMA@users.noreply.github.com>
2024-11-14 02:33:00 +0100

    Update Bincom.js

--- Bincom.js
+++ Bincom.js
@@ -1,1 +1,2 @@
 function button1(){document.getElementById('seeless').innerHTML= " ●	B.ENG (Electrical and Electronics Engineering) – Second class upper division"}
+function button2(){document.getElementById('seemore').innerHTML= 'SEE MORE'}
